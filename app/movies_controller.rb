3cfc8cc23bb90b308aa251c7a7648ac43400f0e0
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-14 20:08:36 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -68,7 +68,7 @@ end
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
-  Movie.find_by(:title => "Awesome Flick")
+  Movie.find(:title => "Awesome Flick")
   Movie.update(:title => "Even Awesomer Flick")
   Movie.save
 end
@@ -76,7 +76,7 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?")
-  Movie.update()
+  Movie.upda
   _
 end
 
